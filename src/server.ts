e51060ded3dd793f60acf6061a457503ca4404cf
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-12-08 10:08:44 -0300

    feat: prepare basics about server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.info("here we go!");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.get("/", (req, res) => {
+    res.send("API IS UP");
+});
+
+io.on('connection', () => {
+    console.info("Socket.io connected!");
+});
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://localhost:27017/trelloclonedb").then(() => {
+    console.log("Connected to mongodb");
+    httpServer.listen(4001, () => {
+        console.info("API is listening on port 4001");
+    });
+});
+
