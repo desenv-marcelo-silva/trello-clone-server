71554d6c6565116ba7bed28a60a1905830ebb5cc
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-12-26 18:01:44 -0300

    feat: Create controller board
    Configure mongoose to remove _id

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 
 import * as usersController from "./controllers/users";
+import * as boardController from './controllers/boards';
+
 import authMiddleware from "./middlewares/auth";
 
 const app = express();
@@ -16,6 +18,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.set("toJSON", {
+    virtuals: true,
+    transform:(_, converted) => {
+        delete converted._id;
+    }
+});
+
 app.get("/", (req, res) => {
     res.send("API IS UP");
 });
@@ -23,6 +32,7 @@ app.get("/", (req, res) => {
 app.post("/api/users", usersController.register);
 app.post("/api/users/login", usersController.login);
 app.get("/api/user", authMiddleware, usersController.currentUser);
+app.get('/api/boards', authMiddleware, boardController.getBoards);
 
 io.on('connection', () => {
     console.info("Socket.io connected!");
